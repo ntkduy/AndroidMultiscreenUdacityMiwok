8d93b00c6e7c019aececc63d0b9d0ff296fe6e6e
ntkduy <ntkduy1604@gmail.com>
2017-02-25 14:12:20 +0800

    Add initial explicit intent to NumbersActivity - Without any data invoke - Modify AndroidManifest.xml: Show name of the NumbersActivity - Add onClick event to Numbers TextView (openNumbersList) in activity_main.xml - Add openNumbersList in MainActivity.java

--- app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/MainActivity.java
+++ app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.ntkduy1604.androidmultiscreenudacitymiwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +26,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view){
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
     }
 }
