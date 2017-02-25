ad8341bad7f88797e5eb50151817eb6f3451e8c4
ntkduy <ntkduy1604@gmail.com>
2017-02-25 16:33:26 +0800

    Use LinearLayout class to display Views from JAVA to XML - ArrayList: How to use ArrayList vs. Array - Review: For & While loops

--- app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/NumbersActivity.java
+++ app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.ntkduy1604.androidmultiscreenudacitymiwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Add array of Words To NumbersActivity
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Find the root view so we can add child views to it
+        LinearLayout  rootView = (LinearLayout) findViewById(R.id.activity_numbers);
+
+        for(int index = 0; index < words.size(); index++){
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+            // Increment the index variable by 1
+        }
     }
 }
