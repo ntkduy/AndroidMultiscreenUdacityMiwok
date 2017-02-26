4f11e6ee5d4afa6d6eaa8df6ea8b6b5c40bb5ed3
ntkduy <ntkduy1604@gmail.com>
2017-02-26 13:14:51 +0800

    Update custom Word & WordAdapter classes - Add images to each translations in Numbers, Family Members and Colors - Modify Word & WordAdapter to distinguish whether there is a need for an image to be displayed with the translation.

--- app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/WordAdapter.java
+++ app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/WordAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -49,11 +50,18 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // set this text on the number TextView
         miworkTextView.setText(currentWord.getMiwokTranslation());
 
-//        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-//        // Get the image resource ID from the current AndroidFlavor object and
-//        // set the image to iconView
-//        iconView.setImageResource(currentWord.getImageResourceId());
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
