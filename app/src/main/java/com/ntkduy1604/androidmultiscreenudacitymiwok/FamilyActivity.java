32953461995387efe382ab64a882bc988762791d
ntkduy <ntkduy1604@gmail.com>
2017-02-26 11:54:11 +0800

    Modify the remaining words (Colors, Family Members, Phrases) - Work well - Update AndroidManifest.xml: change the app label to particular task. - Update all layouts - Update all java codes

--- app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/FamilyActivity.java
+++ app/src/main/java/com/ntkduy1604/androidmultiscreenudacitymiwok/FamilyActivity.java
@@ -2,6 +2,9 @@ package com.ntkduy1604.androidmultiscreenudacitymiwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        //Add array of Words To NumbersActivity
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+
+
+        // Create an ArrayAdapter variable with String data type
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        //Create a ListView variable
+        ListView listView = (ListView) findViewById(R.id.activity_family);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
